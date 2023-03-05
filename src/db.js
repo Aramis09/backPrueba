967c3626e6ee32ce2fa63db526fd64702e263849
Aramis09 <aramisjaime48@gmail.com>
2023-03-05 04:43:39 -0300

    upload

---
+++ src/db.js
@@ -0,0 +1,53 @@
+//**Crea la conexion con la base de datos (con sequelize) */
+const {Sequelize} = require("sequelize");
+require("dotenv").config(); //**La variables de entorno quedan dispobnibles .env */
+const {DB_DATA}= process.env;
+const sequelize = new Sequelize(DB_DATA,{logging:false});
+//las lineas 7 y 8 es para que pueda correrlo en mi pc, agradeceria que lo dejen comentado nomas (nicrus27)
+// const {DB_USER, DB_PASSWORD, DB_HOST, DB_NAME}= process.env;
+// const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_NAME}`, {
+//     logging: false,
+// });
+
+//**Definicion de modelos (con sequelize)*/
+const ProductModel = require("./models/Product");
+const PlatformModel = require("./models/Platform");
+const GenreModel = require("./models/Genre");
+const ImageModel = require("./models/Image");
+const StoreModel = require("./models/Store");
+const ProductsPlatformsModel = require("./models/ProductsPlatforms");
+const ProductsGenresModel = require("./models/ProductsGenres");
+const ProductsStoresModel = require("./models/ProductsStores");
+
+/**Instancias que definen los modelos, crea el .models: */
+ProductModel(sequelize);
+PlatformModel(sequelize);
+GenreModel(sequelize);
+ImageModel(sequelize);
+StoreModel(sequelize);
+ProductsPlatformsModel(sequelize);
+ProductsGenresModel(sequelize);
+ProductsStoresModel(sequelize);
+
+
+
+//**Relacionar los Modelos */
+const {Product, Platform, Genre, Image, Store, ProductsPlatforms, ProductsGenres, ProductsStores} = sequelize.models;
+
+const ProductsPlatforms_Profile = sequelize.define('ProductsPlatforms', {}, { timestamps: false });
+Product.belongsToMany(Platform,{through:ProductsPlatforms_Profile});
+Platform.belongsToMany(Product,{through:ProductsPlatforms_Profile});
+
+const ProductsGenres_Profile = sequelize.define('ProductsGenres', {}, { timestamps: false });
+Product.belongsToMany(Genre,{through:ProductsGenres_Profile});
+Genre.belongsToMany(Product,{through:ProductsGenres_Profile});
+
+const ProductsStores_Profile = sequelize.define('ProductsStores', {}, { timestamps: false });
+Product.belongsToMany(Store,{through:ProductsStores_Profile});
+Store.belongsToMany(Product,{through:ProductsStores_Profile});
+
+Product.hasMany(Image);
+Image.belongsTo(Product);
+
+//**Exportarla para poder trabajar con los modelos en los controllers */
+module.exports={sequelize, ...sequelize.models};
